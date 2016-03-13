8940989dc7f64ecd6df126b4b9fd6b163155e799
Jesse Thach <jessethach@users.noreply.github.com>
2016-03-13 11:40:14 -0700

    Removed commented out line

--- server/routes/public_routes.js
+++ server/routes/public_routes.js
@@ -7,7 +7,6 @@ const handleDBError = require(__dirname + '/../lib/handle_db_error');
 const router = module.exports = exports = express.Router();
 
 router.get('/currentuser', jsonParser, jwtAuth, (req, res) => {
-  // console.log(req)
   User.findOne({_id: req.user.id}, (err, data) => {
 
     if (err) return handleDBError(err, res);
